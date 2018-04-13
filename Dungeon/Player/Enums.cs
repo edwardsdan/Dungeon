a6d035978f6780cd96a81a33be82411643a15f8c
DanE <edwardsdan001@gmail.com>
2018-04-13 00:19:42 -0400

    Initial commit

---
+++ Dungeon/Player/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon.Player
+{
+    public enum Race { human = 0, elf, orc, dwarf, hobbit, goblin }
+    public enum ArcherWeapon { bow = 0, crossbow }
+    public enum KnightWeapon { sword = 0, mace, axe, flail }
+}
